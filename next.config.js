d4d435fb8e433f1664b981d3cf88c317d1bed492
Swabhan Katkoori <46286853+Swabhan@users.noreply.github.com>
2023-10-04 19:33:38 -0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -2,45 +2,13 @@
 const nextConfig = {
   rewrites: async () => {
     return [
-
       {
-        source: "/upcoming",
+        source: "/api/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/upcoming"
+            ? "http://127.0.0.1:8000/api/:path*"
             : "/api/",
       },
-      {
-        source: "/past",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/past"
-            : "/api/",
-      },
-      {
-        source: "/pictures",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/pictures"
-            : "/api/",
-      },
-      {
-        source: "/contact",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/contact"
-            : "/api/",
-      },
-
-      {
-        source: "/about_us",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/about_us"
-            : "/api/",
-      },
-
-
       {
         source: "/docs",
         destination:

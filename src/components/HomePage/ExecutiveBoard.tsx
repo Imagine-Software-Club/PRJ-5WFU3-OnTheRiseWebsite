610abb0aa06174e38b81e0838dfc26f367a086bb
AaronElk4124 <aaronelkin0@gmail.com>
2023-11-14 19:33:56 -0500

    Initial finishing touches

--- src/components/HomePage/ExecutiveBoard.tsx
+++ src/components/HomePage/ExecutiveBoard.tsx
@@ -3,45 +3,51 @@ import React from "react";
 
 import MemberCard from "../Cards/MemberCard";
 
-
 interface IExecutiveBoard {}
 
-{/* Function to Fetch Data */}
+{
+  /* Function to Fetch Data */
+}
 async function getData() {
-  const res = await fetch('http://127.0.0.1:8000/members');
+  const res = await fetch("http://127.0.0.1:8000/members");
   if (!res.ok) {
-    throw new Error('Failed to fetch data')
+    throw new Error("Failed to fetch data");
   }
- 
-  return res.json()
+
+  return res.json();
 }
 
 export default async function ExecutiveBoard() {
-  {/* Fetch OTR Members Info */}
-  const data = await getData()
+  {
+    /* Fetch OTR Members Info */
+  }
+  const data = await getData();
 
-  const otrMembers = data['OTR Members'];
+  const otrMembers = data["OTR Members"];
 
-  const memberCards = otrMembers.map((member, index) => {
-    const name = member['Name'];
-    const role = member['Role'];
-
+  const memberCards = otrMembers.map((member: any, index: number) => {
+    const name = member["Name"];
+    const role = member["Role"];
+    console.log(typeof member);
     if (name) {
       return <MemberCard key={index} name={name} position={role} />;
     }
     return null;
   });
 
-  {/* Style Page */}
-  {/* Utilizes the Member Card Component in /src/components/cards */}
+  {
+    /* Style Page */
+  }
+  {
+    /* Utilizes the Member Card Component in /src/components/cards */
+  }
   return (
     <Box display="flex" justifyContent="space-between" margin="0 -10px">
-      {memberCards.slice(0, 3).map((card, index) => (
+      {memberCards.slice(0, 3).map((card: any, index: number) => (
         <Box key={index} margin="0 100px">
           {card}
         </Box>
       ))}
     </Box>
   );
-
-};
+}
--- src/components/HomePage/OurMission.tsx
+++ src/components/HomePage/OurMission.tsx
@@ -1,13 +1,82 @@
-import { Box } from "@mui/material";
+import { Box, Paper, Stack, Typography } from "@mui/material";
 import React from "react";
+import OnTheRise from "../../../public/images/ontherisegroup.png"; // Update with the correct path to your image
+import Image from "next/image";
+import { Acme } from "next/font/google";
+interface IOurMissionProps {
+  // ... your props if needed
+}
 
-//use this is data needs to be passed into component
-interface IOurMission {}
+const acme = Acme({
+  weight: ["400"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
-export const OurMission: React.FC<IOurMission> = () => {
-  //Change this code
-  let hi;
-  return <Box>Hi</Box>;
+const EventCard = () => {
+  return (
+    <Stack
+      direction="column"
+      spacing={1}
+      alignItems="center"
+      sx={{ flexGrow: 1, flexBasis: "50%" }}
+    >
+      <Image src={OnTheRise} alt="Event" width={150} height={150} />{" "}
+      <Typography sx={{ fontSize: "24px", mt: 1 }}>
+        Event Description
+      </Typography>
+    </Stack>
+  );
+};
+
+const OurMission: React.FC<IOurMissionProps> = () => {
+  // ... your data and useEffect if needed
+
+  return (
+    <Stack sx={{ mb: "10%", width: "100%" }}>
+      <Typography
+        sx={{
+          fontSize: "50px",
+          fontFamily: acme.style.fontFamily,
+          mb: "5%",
+          textAlign: "center",
+        }}
+      >
+        Our Mission
+      </Typography>
+      <Stack direction="row" spacing={2}>
+        <Paper sx={{ width: "60%" }}>
+          <Box
+            sx={{
+              background: "#EFD80A",
+              p: 2,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              fontSize: "24px",
+              fontFamily: acme.style.fontFamily,
+            }}
+          >
+            Events
+          </Box>
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            sx={{ p: 2, flexWrap: "wrap" }}
+          >
+            <EventCard />
+            <EventCard />
+            <EventCard />
+            <EventCard />
+          </Stack>
+        </Paper>
+        <Paper sx={{ width: "40%" }}>
+          {/* Additional content for the second paper */}
+        </Paper>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default OurMission;

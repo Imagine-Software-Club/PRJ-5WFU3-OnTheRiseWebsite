0353907ad0d2eda5c2fc9e53cc8c22b9be5d0ab7
otrmsuwebsite <156859687+otrmsuwebsite@users.noreply.github.com>
2024-01-23 17:32:05 -0500

    Handle CORS

--- next.config.js
+++ next.config.js
@@ -1,65 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  rewrites: async () => {
+module.exports = {
+  async headers() {
     return [
       {
-        source: "/upcoming",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/upcoming"
-            : "/api/",
-      },
-      {
-        source: "/members",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/members"
-            : "/api/",
-      },
-      {
-        source: "/past",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/past"
-            : "/api/",
-      },
-      {
-        source: "/profiles",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/profiles"
-            : "/api/",
-      },
-      {
-        source: "/upcoming/post",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/upcoming/post"
-            : "/api/docs",
-      },
-      {
-        source: "/docs",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/docs"
-            : "/api/docs",
-      },
-      {
-        source: "/openapi.json",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/openapi.json"
-            : "/api/openapi.json",
-      },
-      {
-        source: "/events/:id",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/event/:id"
-            : "/api/",
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
       },
     ];
   },
 };
-
-module.exports = nextConfig;
